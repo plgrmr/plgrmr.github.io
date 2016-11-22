76f84d0be4014757b3f04faaebfc3f1eeed16896
plgrmr <philogrammer+1@gmail.com>
2016-11-22 15:15:00 +0900

    test
    test

---
+++ _source/image-test/image-test.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/image-test/2016-11-22-image-test.Rmd
+
---
+++ _source/review/review.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/review/2016-11-22-review.Rmd
+
