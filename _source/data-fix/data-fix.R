b59a117a5c004f43c9b8e39d1f4d3a9ae5a9c6dd
plgrmr <philogrammer+1@gmail.com>
2017-03-15 17:30:31 +0900

    encoding_post
    encoding_post

---
+++ _source/data-fix/data-fix.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/data-fix/2017-02-18-data-fix.Rmd
+
---
+++ _source/headfirst-statistics-ch1/headfirst-statistics-ch1.R
@@ -0,0 +1,3 @@
+# This R file accomanies the .Rmd blog post
+# _source/headfirst-statistics-ch1/2017-01-10-headfirst-statistics-ch1.Rmd
+
